01009bd3e732d0fc416caa659a4dfa43d7b48098
Fernando Sousa <fernando@sousafrc.com>
2024-06-11 20:46:44 -0300

    :sparkles: feat: Spreadsheets are now created following the name pattern mm-dd-yyyy_HH-MM-SS

--- modules/save_to_csv.py
+++ modules/save_to_csv.py
@@ -1,6 +1,14 @@
 import pandas as pd
+from datetime import datetime
 
-def save_delegators_to_csv(delegators, filename="data/delegators.csv"):
+def save_delegators_to_csv(delegators):
+    # Obter a data e hora atual
+    now = datetime.now()
+    # Formatar a data e hora no formato especificado
+    timestamp = now.strftime("pd_%m-%d-%Y_%H-%M-%S")
+    filename = f"data/{timestamp}.csv"
+    
+    # Salvar o DataFrame como CSV
     df = pd.DataFrame(delegators)
     df.to_csv(filename, index=False)
     print(f"Lista de delegadores salva com sucesso em '{filename}'.")
